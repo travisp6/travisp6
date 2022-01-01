9bc5bf4c6686a6d4a8241a80f40fbe3d9fef7b0a
travisp6 <96930736+travisp6@users.noreply.github.com>
2022-01-01 00:08:02 -0500

    Create README.py

---
+++ README.py
@@ -0,0 +1,40 @@
+- 👋 Hi, I’m @travisp6
+
+def greetings(viewer):
+    if viewer == 'Nice':
+        print('Hello! Nice to meet you.')
+    else:
+        pass
+greetings('Nice')
+
+- 👀 I’m interested in
+
+a=[]
+for i in ('fantasy books','Stargate SG1', 'computers'):
+    a.append(i)
+print(a)
+
+- 🌱 I’m currently learning
+
+mystery = ['a','w','s','p','y','t','h','o','n','k','a','f','k','a','e','k','s']
+while len(mystery) >= 1:
+    if len(mystery) > 14:
+        print(*mystery[0:3],sep='')
+        print(*mystery[14:],sep='')
+        del mystery[:3];del mystery[-3:]
+    else:
+        print(*mystery[6:],sep='')
+        print(*mystery[:-5],sep='')
+        break
+
+- 💞️ I’m looking to collaborate on
+
+class Collab():
+    def __init__(self, web_building, coding, cloud_compute):
+        self.web_building = web_building
+        self.coding = coding
+        self.cloud_compute = cloud_compute
+
+- 📫 How to reach me ...
+
+Message me
